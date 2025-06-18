fe22777b77b1b090c302d09aed9c40780f31be29
Gabriel Costa <gabrielcdacosta@gmail.com>
2025-06-18 20:36:01 -0300

    initial frontend commit

---
+++ src/test/java/com/campusplus/campusplus/CampusplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusplus.campusplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
